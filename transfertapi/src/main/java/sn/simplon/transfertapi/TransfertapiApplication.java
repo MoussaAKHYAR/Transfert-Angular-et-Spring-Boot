389eaa778193d2740c81d953841418fd3e36f893
MoussaAKHYAR <amrane93.mb@gmail.com>
2020-11-24 15:03:46 +0000

    creation du projet spring boot pour l'api

---
+++ transfertapi/src/main/java/sn/simplon/transfertapi/TransfertapiApplication.java
@@ -0,0 +1,13 @@
+package sn.simplon.transfertapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransfertapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransfertapiApplication.class, args);
+    }
+
+}
---
+++ transfertapi/src/test/java/sn/simplon/transfertapi/TransfertapiApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.simplon.transfertapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransfertapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
